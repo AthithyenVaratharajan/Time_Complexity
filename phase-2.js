7123fe63ae02129343811cc34f0b01d589f7fec0
Athithyen <athithyen11@gmail.com>
2022-06-25 19:22:05 -0400

    Time-Complexity

--- phase-2.js
+++ phase-2.js
@@ -2,14 +2,21 @@ const [addNums, addManyNums] = require("./phase-1");
 
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  let array = [];
+  for ( let i = increment; i <= increment*10; i+=increment){
+    array.push(addNums(i));
+  }
+  return array;
 
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
-
+  let newArray = [];
+  for ( let i = increment; i <=increment*10; i+=increment){
+    newArray.push(addManyNums(i));
+  }
+  return newArray;
 }
 
 module.exports = [addNums10, addManyNums10];
--- phase-3.js
+++ phase-3.js
@@ -1,7 +1,17 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
+  function addNums(n) {
+    let sum = 0;
+    for ( let i = 1; i <= n; i++){
+      startTime = Date.now();
+      sum += i;
+      endTime = Date.now();
+      console.log(`${endTime - startTime}`);
+    }
+    return sum;
+
+  }
   // Then, add timing code
 
   // Your code here
@@ -10,20 +20,35 @@ function addNums10Timing(increment) {
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
+  function addManyNums(n) {
+    let sum = 0;
+    // Fill this in
+    for ( let i = 1; i <= n; i++){
+      startTime = Date.now();
+      sum += addNums(i);
+      endTime = Date.now();
+      console.log(`${endTime - startTime}`);
+    }
+    return sum;
+  }
 // Then, add timing code
 
   // Your code here
 
 }
 
-
+console.time("addNums10Timing");
 n = 1000000
 console.log(`addNums(${n}): `);
 addNums10Timing(1000000);
+console.timeEnd("addNums10Timing");
+console.timeLog("addNums10Timing");
+
 
 console.log("\n***********\n");
-
-n = 1000
+console.time("addManyNums10Timing");
+n = 5000
 console.log(`addManyNums(${n}): `);
 addManyNums10Timing(5000);
+console.timeEnd("addManyNums10Timing");
+console.timeLog("addManyNums10Timing");
